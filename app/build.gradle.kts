80ba6c204b8a157b08c55aa7caaf7bc5b6dd2ae6
mwanikukala-rgb <mwanikukala@gmail.com>
2026-08-08 16:20:01 +0200

    Set up Android build configuration in build.gradle.kts
    Configured Android build settings including SDK versions, application ID, and source sets.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,34 @@
 
+android {
+    namespace = "com.bemba.tts"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.bemba.tts"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    sourceSets {
+        getByName("main") {
+            assets.srcDirs("../../models")
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
