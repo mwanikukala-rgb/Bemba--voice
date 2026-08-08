6ca87f6c16988981a7bbdd67de351207cb0273c9
mwanikukala-rgb <mwanikukala@gmail.com>
2026-08-08 16:22:12 +0200

    Update build.gradle.kts to modify sourceSets and dependencies
    Removed sourceSets configuration and added dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
 android {
     namespace = "com.bemba.tts"
@@ -9,12 +13,6 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-    }
-
-    sourceSets {
-        getByName("main") {
-            assets.srcDirs("../../models")
-        }
     }
 
     buildTypes {
@@ -31,4 +29,11 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+}
+
+dependencies {
+    implementation("com.microsoft.onnxruntime:onnxruntime-android:1.20.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
